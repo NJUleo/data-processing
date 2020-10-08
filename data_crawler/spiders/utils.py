32bfb48f8054069667c523efae55bfd749f7fbde
NJUleo <1250925329@qq.com>
2020-10-08 15:59:57 +0800

    feat(spiders/utils): 不再默认保存的文件是html文件

--- data_crawler/spiders/utils.py
+++ data_crawler/spiders/utils.py
@@ -23,11 +23,11 @@ def get_keywords(paper):
 
 # 通过文件保存一些内容，仅用于测试 TODO: 之后需要改成正式的方式保存
 def save_byte_file(content, name = 'test_byte'):
-    filename = 'test_files/' + name + '.html'
+    filename = 'test_files/' + name
     with open(filename, 'wb') as f:
         f.write(content)
 
 def save_str_file(content, name = 'test_str'):
-    filename = 'test_files/' + name + '.html'
+    filename = 'test_files/' + name
     with open(filename, 'w') as f:
         f.write(content)
