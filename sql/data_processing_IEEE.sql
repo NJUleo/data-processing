886b593aee50e8f43ba9bcc81a5eb0abfeb4eec8
NJUleo <1250925329@qq.com>
2020-11-24 09:30:49 +0800

    refactor(sql): 将不同库的 sql 分开保存，且不在其中定义库名。

---
+++ sql/data_processing_IEEE.sql
@@ -0,0 +1,78 @@
+DROP TABLE IF EXISTS `paper`;
+CREATE TABLE `paper` (
+    `id` varchar(255) NOT NULL,
+    `title` varchar(4095) COLLATE utf8_bin NOT NULL,
+    `abs` varchar(8191) COLLATE utf8_bin NOT NULL,
+    `publication_id` varchar(255) COLLATE utf8_bin NOT NULL,
+    `publication_date` varchar(255) COLLATE utf8_bin NOT NULL,
+    `link` varchar(255) COLLATE utf8_bin,
+    `doi` varchar(255) COLLATE utf8_bin,
+    `citation` int(11) NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+DROP TABLE IF EXISTS `domain`;
+CREATE TABLE `domain` (
+    `id` varchar(255) COLLATE utf8_bin NOT NULL,
+    `name` varchar(255) COLLATE utf8_bin NOT NULL,
+    `url` varchar(255) COLLATE utf8_bin,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+DROP TABLE IF EXISTS `affiliation`;
+CREATE TABLE `affiliation` (
+    `id` varchar(255) NOT NULL,
+    `name` varchar(255) COLLATE utf8_bin NOT NULL,
+    `description` varchar(4095) COLLATE utf8_bin,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+DROP TABLE IF EXISTS `publication`;
+CREATE TABLE `publication` (
+    `id` varchar(255) NOT NULL,
+    `name` varchar(255) COLLATE utf8_bin NOT NULL,
+    `publication_date` varchar(255) COLLATE utf8_bin NOT NULL,
+    `impact` varchar(255) COLLATE utf8_bin DEFAULT '-1',
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+DROP TABLE IF EXISTS `researcher`;
+CREATE TABLE `researcher` (
+    `id` varchar(255) NOT NULL,
+    `name` varchar(255) COLLATE utf8_bin NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+DROP TABLE IF EXISTS `paper_researcher`;
+CREATE TABLE `paper_researcher` (
+    `pid` varchar(255) COLLATE utf8_bin NOT NULL,
+    `rid` varchar(255) COLLATE utf8_bin NOT NULL,
+    `order` varchar(255) COLLATE utf8_bin NOT NULL,
+    PRIMARY KEY (`pid`, `rid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+DROP TABLE IF EXISTS `paper_reference`;
+CREATE TABLE `paper_reference` (
+    `pid` varchar(255) COLLATE utf8_bin NOT NULL,
+    `order` int(11) NOT NULL,
+    `r_doi` varchar(255) COLLATE utf8_bin,
+    `r_title` varchar(4095) COLLATE utf8_bin,
+    `r_document_id` varchar(255) COLLATE utf8_bin,
+    `r_citation` varchar(4095) COLLATE utf8_bin,
+    PRIMARY KEY (`pid`, `order`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+DROP TABLE IF EXISTS `paper_domain`;
+CREATE TABLE `paper_domain` (
+    `pid` varchar(255) COLLATE utf8_bin NOT NULL,
+    `did` varchar(255) COLLATE utf8_bin NOT NULL,
+    PRIMARY KEY (`pid`, `did`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+DROP TABLE IF EXISTS `researcher_affiliation`;
+CREATE TABLE `researcher_affiliation` (
+    `rid` varchar(255) COLLATE utf8_bin NOT NULL,
+    `aid` varchar(255) COLLATE utf8_bin NOT NULL,
+    `year` varchar(255) COLLATE utf8_bin NOT NULL,
+    PRIMARY KEY (`rid`, `aid`, `year`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
