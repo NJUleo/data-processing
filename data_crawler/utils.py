6c5bda90255cc0e753f285970ec16b92de9359a1
NJUleo <1250925329@qq.com>
2020-12-21 13:53:12 +0800

    feat(utils): remove html

--- data_crawler/utils.py
+++ data_crawler/utils.py
@@ -1,5 +1,12 @@
 import hashlib
+import re
 def hash_str(s):
     hl = hashlib.md5() # 如果是同一个 md5 对象然后多次 update 则得到的结果不同
     hl.update(s.encode())
     return hl.hexdigest()
+
+def remove_html(string):
+    html_tag_pattern = re.compile(r'<[^>]+>')
+    alt_tag_pattern = re.compile(r'<alternatives>.*</alternatives>')
+    str_no_alt = re.sub(alt_tag_pattern, '', string)
+    return (re.sub(html_tag_pattern, '', str_no_alt).replace('\n', '').replace('  ', '')).strip()
